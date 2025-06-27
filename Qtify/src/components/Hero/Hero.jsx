d2f7ecc7cc1a55178ca841abdb8b5e26e4685007
Prasad Kulkarni <dev.prasad.kulkarni@gmail.com>
2025-06-27 13:46:46 +0530

    hero-text,image

--- Qtify/src/components/Hero/Hero.jsx
+++ Qtify/src/components/Hero/Hero.jsx
@@ -1,12 +1,21 @@
 import React from "react";
+import Box from "@mui/material/Box";
+import { useTheme } from "@mui/material/styles";
+
 import styles from "./Hero.module.css";
 import heroImage from "../../assets/heroImage.svg";
 
 const Hero = () => {
+  const theme = useTheme();
+
   return (
-    <div className={styles.heroCenter}>
+    <Box className={styles.heroCenter}>
+      <div className={styles.font}>
+        <h1>100 thousand songs, ad free</h1>
+        <h1>over thousands podcast episodes</h1>
+      </div>
       <img src={heroImage} alt="heroImage"></img>
-    </div>
+    </Box>
   );
 };
 
--- Qtify/src/components/Navbar/Button/Button.jsx
+++ Qtify/src/components/Navbar/Button/Button.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import Button from "@mui/material/Button";
+import Box from "@mui/material/Box";
+import { useTheme } from "@mui/material/styles";
+import styles from "./Button.module.css";
 
-const Button = () => {
-  return <div>Give Feedback</div>;
+const ButtonMui = () => {
+  const theme = useTheme();
+
+  return (
+    <Box sx={{ display: "flex", alignItems: "center" }}>
+      <Button
+        className={styles.font}
+        variant="contained"
+        sx={{
+          backgroundColor: theme.palette.secondary.main,
+          color: theme.palette.primary.main,
+        }}
+      >
+        Give Feedback
+      </Button>
+    </Box>
+  );
 };
 
-export default Button;
+export default ButtonMui;
